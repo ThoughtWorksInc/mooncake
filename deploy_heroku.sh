ea757e182583a66001351ca41fffa11066f5e3fc
Rob Speller <rspeller@thoughtworks.com>
2015-08-11 16:26:53 +0100

    DM + RS | 129 | Update deploy_heroku.sh to clear environment variables before setting them

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -7,6 +7,8 @@ heroku maintenance:on
 heroku buildpacks:clear
 heroku buildpacks:set https://github.com/heroku/heroku-buildpack-clojure
 heroku buildpacks:add --index 1 https://github.com/heroku/heroku-buildpack-nodejs
+heroku config -s | awk -F "=" '{print $1}' ORS=' ' | xargs heroku config:unset
+echo "Setting environment variables and suppressing output"
 cat mooncake-staging.env | xargs heroku config:set > /dev/null 2> /dev/null
 git push heroku master
 heroku maintenance:off
