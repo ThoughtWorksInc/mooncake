6c069bacda5ea55bfd764c41988bd4f7f2cd4648
Rob Speller <rspeller@thoughtworks.com>
2015-08-11 15:22:28 +0100

    DM + RS | 129 | Set environment vars on heroku deployment from snap secure file

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -7,6 +7,7 @@ heroku maintenance:on
 heroku buildpacks:clear
 heroku buildpacks:set https://github.com/heroku/heroku-buildpack-clojure
 heroku buildpacks:add --index 1 https://github.com/heroku/heroku-buildpack-nodejs
+cat mooncake-staging.env | xargs heroku config:set
 git push heroku master
 heroku maintenance:off
 
