637c96a4fcfdb7668a0371f778e724b022532bff
Rob Speller <rspeller@thoughtworks.com>
2015-08-11 16:58:07 +0100

    DM + RS | 129 | Update deploy_heroku.sh for setting/unsetting env vars

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -7,9 +7,9 @@ heroku maintenance:on
 heroku buildpacks:clear
 heroku buildpacks:set https://github.com/heroku/heroku-buildpack-clojure
 heroku buildpacks:add --index 1 https://github.com/heroku/heroku-buildpack-nodejs
-heroku config -s | awk -F "=" '{print $1}' ORS=' ' | xargs heroku config:unset
-echo "Setting environment variables and suppressing output"
-cat mooncake-staging.env | xargs heroku config:set > /dev/null 2> /dev/null
+heroku config:unset $(heroku config -s | awk -F "=" '{print $1}' ORS=' ')
+echo "Setting heroku environment variables and suppressing output"
+heroku config:set $(cat mooncake-staging.env | awk '{print}' ORS=' ') &> /dev/null
 git push heroku master
 heroku maintenance:off
 
