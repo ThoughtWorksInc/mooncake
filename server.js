32ed19852c393c9e6b7998d6929aa19a28c6a196
Duncan Mortimer <dmortime@thoughtworks.com>
2015-08-12 13:58:56 +0100

    DM + RS | 148 | Include sign-in in gulp server

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.get('/', function(req, res){
   res.render('index', pageData);
 });
 
+app.get('/sign-in', function(req, res){
+  res.render('sign-in', pageData);
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
