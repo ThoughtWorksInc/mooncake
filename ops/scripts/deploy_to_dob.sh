23f7bda6301fff923f7b726055bbfc22640f36e3
Ra-el Peters <rpeters@thoughtworks.com>
2015-08-14 11:51:43 +0100

    RP + AB | 145 | remove commented code

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -4,8 +4,6 @@ ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/target"
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/config"
 scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/mooncake/target/mooncake-standalone.jar
 scp mooncake-$SNAP_STAGE_NAME.env $REMOTE_USER@$SERVER_IP:/var/mooncake/config/mooncake.env
-#scp logo.svg $REMOTE_USER@$SERVER_IP:/data/mooncake/static/logo.svg
-#scp dcent-favicon.ico $REMOTE_USER@$SERVER_IP:/data/mooncake/static/dcent-favicon.ico
 ssh $REMOTE_USER@$SERVER_IP <<EOF
   sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
