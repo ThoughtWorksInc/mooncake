609dcabca39a92f702de19dad059c362d557bfcf
John Cowie <jcowie@thoughtworks.com>
2015-10-16 14:45:41 +0100

    JC | #238 | Fix deployment script to scp activity sources file

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -4,7 +4,7 @@ ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/target"
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/config"
 scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/mooncake/target/mooncake-standalone.jar
 scp mooncake.env $REMOTE_USER@$SERVER_IP:/var/mooncake/config/mooncake.env
-scp activity-sources.yml $REMOTE_USER@SERVER_IP:/var/mooncake/config/activity-sources.yml
+scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-sources.yml
 ssh $REMOTE_USER@$SERVER_IP <<EOF
   sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
