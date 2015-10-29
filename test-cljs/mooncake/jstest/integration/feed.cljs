389d9ffa74b9a0ca0b9d4a44cc4d91fad952be69
aewelch <aewelch@hotmail.co.uk>
2015-10-29 17:48:36 +0000

    JC + AW | 228 | added extra assertions to loading more activities test

--- test-cljs/mooncake/jstest/integration/feed.cljs
+++ test-cljs/mooncake/jstest/integration/feed.cljs
@@ -2,7 +2,8 @@
   (:require [cemerick.cljs.test]
             [mooncake.jstest.test-utils :as tu]
             [mooncake.js.app :as app]
-            [mooncake.js.feed :as feed])
+            [mooncake.js.feed :as feed]
+            [dommy.core :as dommy])
   (:require-macros [cemerick.cljs.test :refer [deftest is testing]]
                    [mooncake.jstest.macros :refer [load-template]]
                    [dommy.core :refer [sel1 sel]]))
@@ -16,11 +17,11 @@
                              "object" {"url" "http://activity-src.co.uk/bob"}
                              "activity-src-no" "1"
                              "action-text" "created an activity"}
-                             {"actor" {"displayName" "Bob"}
-                              "limited-title" "Bob's Activity"
+                             {"actor" {"displayName" "Bill"}
+                              "limited-title" "Bill's Activity"
                               "formatted-time" "2 weeks ago"
                               "published" "2016-12-11T01:24:45.192Z"
-                              "object" {"url" "http://activity-src.co.uk/bob"}
+                              "object" {"url" "http://activity-src.co.uk/bill"}
                               "activity-src-no" "1"
                               "action-text" "created an activity"}]})
 
@@ -41,4 +42,9 @@
                   (set-initial-state)
                   (is (= 14 (count (sel :.clj--activity-item))))
                   (feed/handler response)
-                  (is (= 16 (count (sel :.clj--activity-item))))))
+                  (let [activity-items (sel :.clj--activity-item)
+                        activity-15 (nth activity-items 14)
+                        activity-16 (nth activity-items 15)]
+                    (is (= 16 (count activity-items)))
+                    (is (= (dommy/text (sel1 activity-15 :.clj--activity-item__title)) "Bob's Activity"))
+                    (is (= (dommy/text (sel1 activity-16 :.clj--activity-item__title)) "Bill's Activity")))))
