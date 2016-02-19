ff64d63bfc25959450818f98ded4b9a21fac432a
Seraphiana <seraphiana@hotmail.co.uk>
2016-02-19 17:09:00 +0000

    FM | 312 | added missing config volume for docker deploy

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -6,7 +6,7 @@ scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-s
 ssh $REMOTE_USER@$SERVER_IP <<EOF
   sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
-  sudo docker run -d \
+  sudo docker run -d -v /var/mooncake/config:/var/mooncake/config \
                   --env-file=/var/mooncake/config/mooncake.env \
                   -p 127.0.0.1:5000:3000 \
                   --link mongo:mongo \
