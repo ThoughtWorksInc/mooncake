fbb212cc9f3e68bc37b38e4b375d691d7afd8a9b
Seraphiana <seraphiana@hotmail.co.uk>
2016-02-19 14:08:34 +0000

    FM | 312 | updated deploy script to use mooncake docker image

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/target"
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/config"
-scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/mooncake/target/mooncake-standalone.jar
 scp mooncake.env $REMOTE_USER@$SERVER_IP:/var/mooncake/config/mooncake.env
 scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-sources.yml
 ssh $REMOTE_USER@$SERVER_IP <<EOF
   sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
-  sudo docker run -d -v /var/mooncake/target:/var/mooncake \
-                     -v /var/mooncake/config:/var/mooncake/config \
-                     -p 127.0.0.1:5000:3000 \
-                     --name mooncake \
-                     --env-file=/var/mooncake/config/mooncake.env \
-                     --link mongo:mongo \
-                     java:8 bash -c 'java -Dlog4j.configuration=log4j.dev -jar /var/mooncake/mooncake-standalone.jar'
+  sudo docker run -d --env-file=/var/mooncake/config/mooncake.env
+                  -p 127.0.0.1:5000:3000
+                  --link mongo:mongo
+                  --name mooncake
+                  dcent/mooncake
 EOF
