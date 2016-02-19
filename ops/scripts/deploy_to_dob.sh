a286b564ff2dbd9456efea68c5b4ec6355a3cb3e
Seraphiana <seraphiana@hotmail.co.uk>
2016-02-19 14:59:07 +0000

    FM | 312 | fixed deploy script

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -6,9 +6,10 @@ scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-s
 ssh $REMOTE_USER@$SERVER_IP <<EOF
   sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
-  sudo docker run -d --env-file=/var/mooncake/config/mooncake.env
-                  -p 127.0.0.1:5000:3000
-                  --link mongo:mongo
-                  --name mooncake
-                  dcent/mooncake
+  sudo docker run -d \
+                  --env-file=/var/mooncake/config/mooncake.env \
+                  -p 127.0.0.1:5000:3000 \
+                  --link mongo:mongo \
+                  --name mooncake \
+                  dcent/mooncake \
 EOF
