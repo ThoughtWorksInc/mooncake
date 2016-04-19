2ded0189fd46d82d3f5a9e9b212e2b31a7e39742
aewelch <amyelwelch@gmail.com>
2016-04-19 17:27:35 +0100

    AW | 377 | updated deploy script copying files by users other than root

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/config"
-scp mooncake.env $REMOTE_USER@$SERVER_IP:/var/mooncake/config/mooncake.env
-scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-sources.yml
+scp mooncake.env $REMOTE_USER@$SERVER_IP:~/mooncake.env
+scp activity-sources.yml $REMOTE_USER@$SERVER_IP:~/activity-sources.yml
 ssh $REMOTE_USER@$SERVER_IP <<EOF
-  echo $REMOTE_PASSWORD | sudo -S docker stop mooncake || echo 'Failed to stop mooncake container'
+  echo $REMOTE_PASSWORD | sudo -S mv ~/mooncake.env /var/mooncake/config/mooncake.env
+  sudo mv ~/activity-sources.yml /var/mooncake/config/activity-sources.yml
+  sudo docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
   sudo docker run -d -v /var/mooncake/config:/var/mooncake/config \
                   --env-file=/var/mooncake/config/mooncake.env \
