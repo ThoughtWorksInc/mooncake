ebeb29125d0c47127c9ab9ad3d396fbcb9f72dca
aewelch <amyelwelch@gmail.com>
2016-04-19 15:28:43 +0100

    AW | 377 | sudo password echo'd in deploy script so that remote user doesn't have to be root

--- ops/scripts/deploy_to_dob.sh
+++ ops/scripts/deploy_to_dob.sh
@@ -4,7 +4,7 @@ ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/mooncake/config"
 scp mooncake.env $REMOTE_USER@$SERVER_IP:/var/mooncake/config/mooncake.env
 scp activity-sources.yml $REMOTE_USER@$SERVER_IP:/var/mooncake/config/activity-sources.yml
 ssh $REMOTE_USER@$SERVER_IP <<EOF
-  sudo docker stop mooncake || echo 'Failed to stop mooncake container'
+  echo $REMOTE_PASSWORD | sudo -S docker stop mooncake || echo 'Failed to stop mooncake container'
   sudo docker rm mooncake || echo 'Failed to remove mooncake container'
   sudo docker run -d -v /var/mooncake/config:/var/mooncake/config \
                   --env-file=/var/mooncake/config/mooncake.env \
